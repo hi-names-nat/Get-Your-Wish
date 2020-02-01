56646f5383f97ea7da12ec3b4d5c18b9c5282053
Gagnon <jg380218@ohio.edu>
2020-01-31 23:47:14 -0500

    Initial Commit

---
+++ Assets/Scripts/TriggerDetect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDetect : MonoBehaviour
+{
+    private Collider c;
+    public GameObject check;
+
+    [System.NonSerialized]
+    public bool triggered = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        c = GetComponent<Collider>();
+        if (!c.isTrigger) {
+            throw new System.Exception("Collider must be a trigger!");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject  == check) {
+            triggered = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+
+        if (other.gameObject == check) {
+            triggered = false;
+        }
+    }
+}
