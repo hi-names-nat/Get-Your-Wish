4d7bcca5f613252563ac7239a96000683eb6b503
Dakota Taylor <54542017+dtaylor00@users.noreply.github.com>
2020-03-04 23:56:52 -0500

    Updated gun to shoot rays
    The gun now shoots out rays instead of gameobjects.
    Can make a gun either automatic or not.
    Added "crosshair" hud
    Bullets affect game objects with rigidbodies depending on their power (gun variable)
    Added props in gun test scene to test out shooting mechanic

--- Assets/Scripts/FPSCharacterGun.cs
+++ Assets/Scripts/FPSCharacterGun.cs
@@ -5,68 +5,78 @@
  * Desc:	    A gun for the player object! Can shoot up to max ammo and can reload whenever not at max ammo. For now, 'F' is to shoot and 'R' is reload
  */
 
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class FPSCharacterGun : MonoBehaviour
-{
+public class FPSCharacterGun : MonoBehaviour {
     [Tooltip("Amount of ammo the gun can hold")]
-    public int maxAmmo = 64;
+    public int maxAmmo = 30;
 
-    [Tooltip("Time (in seconds) it takes to shoot one bullet")]
-    public float fireRate = 0.1f; // in seconds for now, might make it int and have it in milliseconds at another point
+    [Tooltip("How much force the bullet exerts when it hits something")]
+    public float power = 10f;
 
-    [Tooltip("The object the gun shoots")]
-    public GameObject bullet = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Projectile.prefab", typeof(GameObject));
+    [Tooltip("Time (in seconds) it takes to shoot another bullet after firing one")]
+    public float fireRate = 0.1f; // in seconds
 
+    [Tooltip("Whether or not if the gun will keep shooting when trigger is held")]
+    public bool isAutomatic = true;
+
+    private Camera cam;
     private int ammo;
     private float fireCooldown;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
+        cam = GetComponentInChildren<Camera>();
         ammo = maxAmmo;
         fireCooldown = 0f;
-
-        // bullet = Resources.Load("Prefabs/Projectile") as GameObject;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        bool canFire = (fireCooldown <= 0f);
-        if (!canFire)
-            fireCooldown -= Time.deltaTime;
-
-        if (canFire && Input.GetKey(KeyCode.F))
+    void Update() {
+        if (canFire() && Input.GetKey(KeyCode.F))
             FireGun();
 
         if (Input.GetKey(KeyCode.R))
             ReloadGun(); // Don't know if this should be its own function or not ¯\_(ツ)_/¯
     }
 
-    void FireGun()
-    {
-        if (ammo != 0)
-        {
-            ammo -= 1;
-            fireCooldown = fireRate;
-            Camera cam = GetComponentInChildren<Camera>();
-            Vector3 pos = cam.transform.position + cam.transform.forward;
-            Quaternion rot = transform.rotation;
+    // Returns whether or not the gun can fire a bullet
+    bool canFire() {
+        bool canFire = (fireCooldown <= 0f);
+        if (!canFire) {
+            fireCooldown -= Time.deltaTime;
+            canFire = (fireCooldown <= 0f);
+        }
 
-            // TODO: despawn new bullets after a few seconds from being shoot
-            // -or- have each gun use list of bullets and reuse the oldest bullet
-            GameObject newBullet = Instantiate(bullet, pos, rot);
-            newBullet.transform.GetComponent<Rigidbody>().AddForce(cam.transform.forward * 500);
+        if (!isAutomatic)
+            canFire = canFire && Input.GetKeyDown(KeyCode.F);
+
+        return canFire;
+    }
+
+    void FireGun() {
+        if (ammo != 0) {
+            ammo--;
+            fireCooldown = fireRate;
+
+            Ray ray = cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0)); // ray to center
+            Debug.DrawRay(ray.origin, ray.direction, Color.red, 5);
+
+            // Just messing with/testing the rays
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                Transform objectHit = hit.transform;
+
+                var rb = objectHit.GetComponent<Rigidbody>();
+                if (rb != null) rb.AddForce(ray.direction * power);
+            }
         }
     }
 
-    void ReloadGun()
-    {
+    void ReloadGun() {
         if (ammo != maxAmmo)
             ammo = maxAmmo;
     }
