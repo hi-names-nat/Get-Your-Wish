dfd4dc737260db4bb1225ae728dee34f1c7741cd
Natalie Soltis <Natalie.Soltis@outlook.com>
2021-08-28 17:34:58 -0400

    Update to 2020.3.13f1, commit of some LD work. Need to fix VR.

---
+++ Assets/Scripts/ButtonTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+//make sure to set those assets to disabled
+public class ButtonTrigger : MonoBehaviour
+{
+    public bool isEnabled;
+
+    private void Update()
+    {
+        if (isEnabled)
+        {
+            GetComponent<Renderer>().enabled = true;
+            GetComponent<Button>().enabled = true;
+        }
+    }
+
+
+
+}
